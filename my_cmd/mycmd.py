d3550c9195d2c8a0238b02d4e6b8028ed63ba16c
unknown <bidaybek96@mail.ru>
2023-01-06 16:11:53 +0600

    cmd

--- my_cmd/mycmd.py
+++ my_cmd/mycmd.py
@@ -1,10 +1,84 @@
+import os
+import shutil
+
+
 class MyCMD:
     cur_dir: str
 
     def __init__(self, cur_dir: str):
         self.cur_dir = cur_dir
 
-    # def change_dir(self):
+    def show_dir(self) -> None:
+        cnt, cnt1 = 0, 0
+        for i in os.scandir(self.cur_dir):
+            if i.is_dir():
+                print(i)
+                cnt += 1
+            if i.is_file():
+                cnt1 += 2
+        print(f'Number of directories: {cnt}\nNumber of files: {cnt1}')
+        
+    def change_dir(self, new_dir=None) -> None:
+        if new_dir is None:
+            self.__change_dir()
+            return
+        try:
+            if self.cur_dir[len(self.cur_dir) - 1] != '/':
+                os.chdir(self.cur_dir + '/' + new_dir)
+                self.cur_dir += '/'
+            else:
+                os.chdir(self.cur_dir + new_dir)
 
-    def __str__(self):
-        return f'{self.cur_dir}:'
+            self.cur_dir += new_dir
+        except:
+            print('No such directory!')
+
+    def remove_dir(self, dir_name):
+        try:
+            size = len(os.listdir(self.cur_dir + '/' + dir_name))
+            if size == 0:
+                os.rmdir(self.cur_dir + '/' + dir_name)
+            else:
+                if input('Directory is not empty. Continue deleting?\ny/n\n') == 'y':
+                    shutil.rmtree(self.cur_dir + '/' + dir_name)
+        except:
+            print("No such directory")
+    
+    def __change_dir(self) -> None:
+        ind = -1
+        for i in range(0 , len(self.cur_dir)):
+            if self.cur_dir[i] == '/':
+                ind = i
+        
+        if ind != -1:
+            os.chdir(self.cur_dir[:ind])
+            self.cur_dir = self.cur_dir[:ind]
+            
+    def del_file(self, f_name):
+        try:
+            os.remove(self.cur_dir + '/' + f_name)
+        except:
+            print('No such file!')
+            
+    def read_file(self, f_name):
+        try:
+            with open(self.cur_dir + '/' + f_name) as f:
+                for l in f.readlines():
+                    print(l, end='')
+                print()
+        except IOError:
+            print('File not found!')
+            
+    def rename(self, name, new_name):
+        try:
+            print(name, new_name)
+            # os.rename(self.cur_dir + '/' + name, new_name)
+        except:
+            print('No such file or directory')
+    
+    def prettify(self):
+        self.cur_dir = self.cur_dir.replace("\ "[0], '/')
+    
+    def __str__(self) -> str:
+        return f'{self.cur_dir}>'
+    
--- my_cmd/tester.py
+++ my_cmd/tester.py
@@ -1,19 +1,46 @@
 import os
 from mycmd import MyCMD
 
+def validate_input(s: list) -> None:
+    if len(s) > 2 and s[0] != 'ren':
+        for i in range(2, len(s)):
+            s[1] += ' ' +  s[i]
+    elif s[0] == 'ren':
+        s[1] = s[1].replace('"', '')
+        s[2] = s[2].replace('"', '')
 
 def init():
     test = MyCMD(r'C:/')
-
+    os.chdir(test.cur_dir)
     while True:
-        print(test)
+        print(test, end = " ")
         comand = input().strip().split()
-        # print(comand)
-        if len(comand) == 1:
-            if comand[0] == 'dir':
-                print(os.listdir(test.__str__()))
-                print('ok')
+        if comand[0] == 'q':
+            break
+        validate_input(comand)
+        match comand[0]:
+            case 'dir':
+                test.show_dir()
+            case 'cd':
+                if comand[1] == '..':
+                    test.change_dir()
+                else:
+                    test.change_dir(comand[1])
+            case 'rm':
+                test.remove_dir(comand[1])
+            case 'del':
+                test.del_file(comand[1])
+            case 'ren':
+                test.rename(comand[1], comand[2])
+            case 'read':
+                test.read_file(comand[1])
+            case _:
+                print('Invalid command!')
+        test.prettify()
 
 
 if __name__ == '__main__':
     init()
+
+
+# C:\Users\User\pp projects\PP1 CM\new_cmd\CMD
